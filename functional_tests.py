c8cef35587206e8caec046449963d9d5326ed230
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-20 20:29:45 -0300

    fist e2e test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Princy has head about a cool new todo app online and goes to check it out
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header to have To-Do list
+        self.assertIn('To-Do', self.browser.title)
+        self.failt('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
