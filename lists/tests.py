9a4565eea6a5b55de9d533a667e6c3187d8705ca
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-22 21:53:00 -0300

    pruned the tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,3 @@
-from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -10,10 +9,6 @@ class HomePageTest(TestCase):
         view = resolve('/')
         self.assertEqual(view.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
